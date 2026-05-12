73118fadc1f50ca4571a2ceb59ca759a5d92fc2a
Misha Savchenko <misha.savchenko@nasa.gov>
2026-05-12 18:39:19 -0500

    parameterized the file

--- drt_behavior/launch/drt_behavior_executor.launch.py
+++ drt_behavior/launch/drt_behavior_executor.launch.py
@@ -20,7 +20,8 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import OpaqueFunction
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
@@ -28,7 +29,10 @@ from ament_index_python.packages import get_package_share_directory
 def launch_setup(context, *args, **kwargs):
 
     # YAML file containing BT executor parameters
-    bt_config = os.path.join(get_package_share_directory("drt_behavior"), "config", "sample_bt_executor.yaml")
+    package_name = LaunchConfiguration("package_name").perform(context)
+    file_name = LaunchConfiguration("file_name").perform(context)
+
+    bt_config = os.path.join(get_package_share_directory(package_name), "config", file_name)
 
     # Behavior executor node
     behavior_executor = Node(
@@ -44,5 +48,20 @@ def launch_setup(context, *args, **kwargs):
 def generate_launch_description():
 
     declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "package_name",
+            default_value="drt_behavior",
+            description="Name of the package in which to search for the config file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "file_name",
+            default_value="sample_bt_executor.yaml",
+            description="Name of the file in the config directory.",
+        )
+    )
 
     return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
