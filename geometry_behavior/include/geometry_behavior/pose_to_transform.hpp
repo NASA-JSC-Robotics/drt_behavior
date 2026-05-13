fa294deabaae9b05d46e330e39ca3286c12487c3
msavchen-nasa <misha.savchenko@nasa.gov>
2026-05-13 12:59:16 -0500

    Remove specific trees (#3)
    * removed bag_pick_up behavior  tree
    
    * added an additional behavior
    
    * reflect new behavior in README
    
    * formatting and removing comment

---
+++ geometry_behavior/include/geometry_behavior/pose_to_transform.hpp
@@ -0,0 +1,52 @@
+/* Copyright (c) 2026, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+#pragma once
+
+#include "behaviortree_cpp/behavior_tree.h"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+
+namespace geometry_behavior
+{
+class PoseToTransform : public BT::SyncActionNode
+{
+public:
+  /**
+   * @brief Constructor for TfLookup behavior
+   *
+   * @param name Name of behavior
+   * @param config BTCPP node config
+   */
+  PoseToTransform(const std::string& name, const BT::NodeConfig& config);
+
+  /**
+   * @brief define provided ports for the behavior
+   *
+   * @return BT::PortsList Ports that the behavior will use
+   */
+  static BT::PortsList providedPorts();
+
+  /**
+   * @brief The portion of the node that actually does the meaningful work
+   *
+   * @return * BT::NodeStatus Status of the node
+   */
+  BT::NodeStatus tick() override;
+};
+};  // namespace geometry_behavior
--- geometry_behavior/src/plugins.cpp
+++ geometry_behavior/src/plugins.cpp
@@ -22,6 +22,7 @@
 #include "geometry_behavior/apply_transform.hpp"
 #include "geometry_behavior/async_tf_lookup.hpp"
 #include "geometry_behavior/create_random_transform.hpp"
+#include "geometry_behavior/pose_to_transform.hpp"
 #include "geometry_behavior/publish_transform.hpp"
 #include "geometry_behavior/transform_to_pose.hpp"
 
@@ -31,6 +32,7 @@ BTCPP_EXPORT void BT_RegisterRosNodeFromPlugin(BT::BehaviorTreeFactory& factory,
   factory.registerNodeType<geometry_behavior::ApplyTransform>("ApplyTransform");
   factory.registerNodeType<geometry_behavior::AsyncTfLookup>("AsyncTfLookup");
   factory.registerNodeType<geometry_behavior::TransformToPose>("TransformToPose");
+  factory.registerNodeType<geometry_behavior::PoseToTransform>("PoseToTransform");
   factory.registerNodeType<geometry_behavior::CreateRandomTransform>("CreateRandomTransform");
   factory.registerNodeType<geometry_behavior::PublishTransform>("PublishTransform");
 }
